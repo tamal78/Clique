0d36a11943f3cdd7756cf812a52855fe57d069eb
Tamal Kundu <tamalkundu2002@gmail.com>
2024-11-04 00:31:28 +0530

    .

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -128,9 +128,6 @@ export const refreshToken = async (req, res) => {
     );
 
     res.cookie("accessToken", accessToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
       maxAge: 15 * 60 * 1000,
     });
 
