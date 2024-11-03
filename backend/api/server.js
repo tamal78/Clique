995c5a84f3cb371c62d573f8b17a8fad9302e274
Tamal Kundu <tamalkundu2002@gmail.com>
2024-11-03 23:32:06 +0530

    cors change

--- backend/api/server.js
+++ backend/api/server.js
@@ -17,14 +17,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: `${process.env.CLIENT_URL}`,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  credentials: true,
-};
-
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
